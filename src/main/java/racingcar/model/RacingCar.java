70518eacb50385acdb6e612caab69958db8a802c
hyunjo <hyunjo10217@gmail.com>
2025-10-27 16:33:18 +0900

    feat(model): 전진 여부 판별

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -8,9 +8,19 @@ public class RacingCar {
     private static final int NAME_MIN_LENGTH = 1;
     private static final int NAME_MAX_LENGTH = 5;
 
+    private static final int MIN_NUMBER_TO_MOVE = 4;
+
     public RacingCar(String name) {
         this.name = name;
         runCount = 0;
+    }
+
+    public void move(int randomNumber) {
+        if (canMove(randomNumber)) runCount++;
+    }
+
+    private boolean canMove(int randomNumber) {
+        return randomNumber >= MIN_NUMBER_TO_MOVE;
     }
 
 }
